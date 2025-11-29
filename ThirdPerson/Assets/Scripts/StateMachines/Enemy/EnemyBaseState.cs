22fa7ea39be9470de7483f203cf31f7e9c9f4b70
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-11-30 00:25:07 +0200

    Added enemy detecting the player.

---
+++ ThirdPerson/Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected EnemyStateMachine stateMachine;
+
+    protected EnemyBaseState(EnemyStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    protected void Move(float deltaTime)
+    {
+        Move(Vector3.zero, deltaTime);
+    }
+
+    protected void Move(Vector3 motion, float deltaTime)
+    {
+        stateMachine.Controller.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+
+    protected bool IsInChaseRange()
+    {
+        float playerDistanceSquare = (stateMachine.Player.transform.position - stateMachine.transform.position).sqrMagnitude;
+
+        return playerDistanceSquare <= stateMachine.PlayerChasingRange * stateMachine.PlayerChasingRange;
+    }
+}
